27e527837ff66eddb7a759dd6d35a69ce4f65579
PoliPavan <polipavan123@gmail.com>
2024-10-02 12:09:35 +0530

    conditional-statements

---
+++ two.js
@@ -0,0 +1,73 @@
+
+// task2
+// var bill = 100;
+// if (bill == 100){
+//     bill -= bill * 0.1;
+// }
+// else if (bill == 200){
+//     bill -= bill * 0.15;
+// }
+// else if (bill == 300){
+//     bill -= bill * 0.2;
+// }
+// else if (bill > 300){
+//     bill -= bill * 0.25;
+// }
+// console.log(bill)
+// task3
+// var marks ="15"
+// var grade;
+// if (marks <= 100 && marks >= 90 ){
+//    grade ="A"
+// }
+// else if (marks < 89 && marks >= 80 ){
+//     grade ="B"
+// }
+// else if (marks < 69 && marks >= 50 ){
+//     grade ="c"
+// }
+// else if (marks < 49 && marks >= 35 ){
+//     grade ="D"
+// } else if(marks<35){
+//     grade="fail"
+// } else{
+//     grade = "Invalid"
+// }
+// console.log(grade)
+// task4
+// var age = 8;
+// var gender = 'female';
+// if (age >= 18){
+//     if (gender == 'female'){
+//         console.log('eligible')
+//     }
+//     else{
+//         console.log('@...')
+//     }
+// } else {
+//     console.log('!important')
+// }
+// var marks = 25;
+// switch( marks<=100 && marks >=90){
+//     case true:console.log('A'); break;
+//     case false:switch (marks<=89 && marks >=80){
+//         case true:console.log('B'); break;
+//         case false:switch (marks<=79 && marks >=60){
+//             case true:console.log('c'); break;
+//             case false:switch (marks<=59 && marks >=35){
+//                 case true:console.log('D'); break;
+//             case false:switch (marks<35){
+//                 case true: console.log('fail');break;
+//                 case false: console.log('Error')
+//             }
+//         }
+      
+//     }
+   
+// }
+
+
+
+
+
+
